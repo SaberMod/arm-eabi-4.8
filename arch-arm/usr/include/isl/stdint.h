48af887c0d3136b20c60f0ca43b80828cc637357
Paul Beeler <pbeeler80@gmail.com>
2015-03-08 04:18:12 -0600

    03-07-2015
    Signed-off-by: Paul Beeler <pbeeler80@gmail.com>

--- arch-arm/usr/include/isl/stdint.h
+++ arch-arm/usr/include/isl/stdint.h
@@ -2,7 +2,7 @@
 #define _ISL_INCLUDE_ISL_STDINT_H 1
 #ifndef _GENERATED_STDINT_H
 #define _GENERATED_STDINT_H "isl 0.14"
-/* generated using gnu compiler gcc (Ubuntu 4.9.1-16ubuntu6) 4.9.1 */
+/* generated using gnu compiler x86_64-linux-gnu-gcc (Ubuntu 4.9.1-16ubuntu6) 4.9.1 */
 #define _STDINT_HAVE_STDINT_H 1
 #include <stdint.h>
 #endif
